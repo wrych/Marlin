9b51a72a0cd3650ab1c504b5c19f84dd2d039081
Andy Moesch <andy.moesch@hotmail.com>
2021-08-24 21:39:15 +0200

    increase x/y/z motor current after losing steps

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -608,9 +608,9 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp  15.91
-    #define DEFAULT_Ki  0.96
-    #define DEFAULT_Kd  66.09
+    #define DEFAULT_Kp  22.73
+    #define DEFAULT_Ki  1.83
+    #define DEFAULT_Kd  70.58
   #endif
 #endif // PIDTEMP
 
--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2684,7 +2684,7 @@
   #define INTERPOLATE      true
 
   #if AXIS_IS_TMC(X)
-    #define X_CURRENT       800        // (mA) RMS current. Multiply by 1.414 for peak current.
+    #define X_CURRENT       1111        // (mA) RMS current. Multiply by 1.414 for peak current.
     #define X_CURRENT_HOME  X_CURRENT  // (mA) RMS current for sensorless homing
     #define X_MICROSTEPS     256        // 0..256
     #define X_RSENSE          0.11
@@ -2704,7 +2704,7 @@
   #endif
 
   #if AXIS_IS_TMC(Y)
-    #define Y_CURRENT       800
+    #define Y_CURRENT       1111
     #define Y_CURRENT_HOME  Y_CURRENT
     #define Y_MICROSTEPS     256
     #define Y_RSENSE          0.11
@@ -2724,7 +2724,7 @@
   #endif
 
   #if AXIS_IS_TMC(Z)
-    #define Z_CURRENT       800
+    #define Z_CURRENT       1111
     #define Z_CURRENT_HOME  Z_CURRENT
     #define Z_MICROSTEPS     256
     #define Z_RSENSE          0.11
